905b7fd2390797a049fb38bc96a2201443e91a79
José Afonso Sebastião <jose.sebastiao3@hotmail.com>
2026-01-27 23:08:33 -0300

    V84 NOVA IDENTIDADE VISUAL

---
+++ web/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // AQUI ESTÁ A MÁGICA: 
+        // Transformando tudo que é "Azul" (blue) em "Verde" (emerald)
+        blue: colors.emerald, 
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
